2b7570cdba07a7861cd82d3b5d6281fbba563d66
MarquisTheCoder <deshawn.m.williams01@gmail.com>
2023-05-25 13:15:39 -0500

    auto save script Thu May 25 13:15:39 CDT 2023

--- src/main/java/com/trucking/chris/Contact/ContactController.java
+++ src/main/java/com/trucking/chris/Contact/ContactController.java
@@ -2,10 +2,11 @@ package com.trucking.chris.Contact;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContactController {
-    @GetMapping("/contact")
+    @RequestMapping("/contact")
     public String Contact(){
         return "contact";
     }
